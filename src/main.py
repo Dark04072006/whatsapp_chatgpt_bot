89aeeb5f6013b8f0535a7372ab63d2e2aca4b51a
Dark04072006 <139165232+Dark04072006@users.noreply.github.com>
2023-12-24 06:35:13 +0300

    Update main.py

--- src/main.py
+++ src/main.py
@@ -1,16 +1,16 @@
 import logging
 
+from bot.core.container import whatsapp_bot
+from bot.handlers.setup import register_handlers
+
 logger = logging.getLogger(__name__)
 
 
+def main() -> None:
+    register_handlers(bot=whatsapp_bot)
+
+    whatsapp_bot.run_forever()
+
+
 if __name__ == "__main__":
-    try:
-        from bot.core.container import whatsapp_bot
-        from bot.handlers.setup import register_handlers
-
-        register_handlers(bot=whatsapp_bot)
-        whatsapp_bot.run_forever()
-        logger.log(logging.INFO, "Бот завершил работу.")
-
-    except KeyboardInterrupt:
-        logger.log(logging.INFO, "Бот остановлен пользователем.")
+    main()
